7067190b6d6fe8f34fc99633a6ccd79999b1c2f4
tomasmoshi <tomas.alosh@gmail.com>
2025-02-25 13:49:40 -0800

    added a sound feature for the users to able to hear their player walk

--- walking_sound.js
+++ walking_sound.js
@@ -1,4 +1,3 @@
-// walkingSound.js
 import * as THREE from "three";
 
 export class WalkingSound {
@@ -11,11 +10,11 @@ export class WalkingSound {
     const audioLoader = new THREE.AudioLoader();
     audioLoader.load("./sounds/walking-sound-effect.mp3", (buffer) => {
       this.walkingSound.setBuffer(buffer);
-      this.walkingSound.setLoop(true); // Loop for continuous walking
-      this.walkingSound.setVolume(0.5); // Adjust volume as needed
+      this.walkingSound.setLoop(true);
+      this.walkingSound.setVolume(0.5); // Adjust volume
     });
   }
-
+  //playing the walking sound when the user moves
   play() {
     if (!this.walkingSound.isPlaying) {
       this.walkingSound.play();
