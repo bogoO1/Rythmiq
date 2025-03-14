108104fe21c8cbf62facc9bf6051d23fd2b0c5cc
Adel Tazhibi <tazhibiad@gmail.com>
2025-03-14 09:47:08 -0700

    added ambient lighting

--- light.js
+++ light.js
@@ -1,10 +1,9 @@
 import * as THREE from "three";
 
 export function addLight(scene) {
-    // Add Ambient Light
-    // const ambientLight = new THREE.AmbientLight(0xffffff, 0.1); // Soft white light
-    // scene.add(ambientLight);
-
+    //Add Ambient Light
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.1); // Soft white light
+    scene.add(ambientLight);
 
     const light1 = new THREE.DirectionalLight(0xffffff, 0.7); // Soft white light
     light1.position.set(50, 50, 50); // Positioned diagonally above one corner of the room
