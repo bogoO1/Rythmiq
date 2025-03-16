8dd1d8b8647964b617b87f7bc1c6e1f9b4ab923a
bogoO1 <124470591+bogoO1@users.noreply.github.com>
2025-03-15 19:51:20 -0700

    Fix Vite Build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/Rythmiq/", // Must match your repo name exactly!
+  base: "/Rythmiq/", // Make sure this matches the GitHub repo name
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  assetsInclude: ["**/*.frag", "**/*.vert"], // Tells Vite to include shader files
 });
